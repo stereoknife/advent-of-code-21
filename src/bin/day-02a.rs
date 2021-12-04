76bdb640eb8ffea4250d8b2556920c1ace7ca28b
kam <11618271+stereoknife@users.noreply.github.com>
2021-12-04 18:53:27 +0200

    day 2

---
+++ src/bin/day-02a.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use regex::Regex;
+
+fn main() {
+	let contents = fs::read_to_string("./src/inputs/input-02.txt").unwrap();
+	
+	let re = Regex::new(r"(?m)^(forward|up|down) (\d+)$").unwrap();
+	
+	let mut x = 0;
+	let mut d = 0;
+	
+	for cap in re.captures_iter(&contents) {
+		
+		let val : i32 = cap[2].parse().unwrap();
+		match &cap[1] {
+			"forward" => x += val,
+			"up" => d -= val,
+			"down" => d += val,
+			_ => {}
+		}
+	}
+	
+	println!("{}", x * d)
+}
---
+++ src/bin/day-02b.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use regex::Regex;
+
+fn main() {
+	let contents = fs::read_to_string("./src/inputs/input-02.txt").unwrap();
+	
+	let re = Regex::new(r"(?m)^(forward|up|down) (\d+)$").unwrap();
+	
+	let mut x = 0;
+	let mut d = 0;
+	let mut a = 0;
+	
+	for cap in re.captures_iter(&contents) {
+		
+		let val : i32 = cap[2].parse().unwrap();
+		match &cap[1] {
+			"forward" => {
+				x += val;
+				d += val * a;
+			},
+			"up" => a -= val,
+			"down" => a += val,
+			_ => {}
+		}
+	}
+	
+	println!("{}", x * d)
+}
